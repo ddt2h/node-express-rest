97c50a5cf804102163ee56b84dd6c1e99ab576bd
ddt2h <halifire@mail.ru>
2024-10-09 10:07:31 +0300

    deletion

--- src/yup-forms/create-task.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup'
-
-export const createTaskSchema = yup.object({
-    title: yup.string().required(),
-    status: yup.string().oneOf(['new', 'in progress', 'completed']).required(),
-    priority: yup.string().notRequired(),
-    dueDate: yup.date().notRequired()
-})
